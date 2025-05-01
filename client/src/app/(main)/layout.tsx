9c06d31ca71b7275908f3e4fcd6e4abb82514682
Mark Wong <markwong.digital@gmail.com>
2025-05-01 14:45:11 -0700

    Build AppLayout

--- client/src/app/(main)/layout.tsx
+++ client/src/app/(main)/layout.tsx
@@ -2,7 +2,7 @@ import { LayoutProps } from "@/types/app/page-props";
 
 function AppLayout({ children }: LayoutProps) {
 	return (
-		<div className="">
+		<div className="flex flex-col w-full h-full">
 			<main className="">{children}</main>
 		</div>
 	);
